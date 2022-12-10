8214124b6e4e84d74700070d87f36f37b29458f4
Džuris <juris@glaive.pro>
2022-12-10 14:59:21 +0200

    Add Katex component

---
+++ src/components/Katex.svelte
@@ -0,0 +1,28 @@
+<script>
+	// source:
+	// https://svelte.dev/repl/49ff6c089825418888cf804d9dde77bc?version=3.54.0
+
+	import katex from 'katex'
+	/** @type string */
+	export let math
+	/** @type boolean */
+	export let displayMode = false
+
+	const options = {
+		displayMode: displayMode,
+		throwOnError: false
+	}
+
+	$: katexString = katex.renderToString(math, options)
+</script>
+
+<svelte:head>
+	<!-- Use https://www.srihash.org/ when updating -->
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.css"
+		integrity="sha384-vKruj+a13U8yHIkAyGgK1J3ArTLzrFGBbBc0tDp4ad/EyewESeXE/Iv67Aj8gKZ0"
+		crossorigin="anonymous" >
+</svelte:head>
+
+{@html katexString}
