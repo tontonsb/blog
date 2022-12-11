3dba96cc511ffef70c7c87cb7590883ff16acf96
Džuris <juris@glaive.pro>
2022-12-11 12:12:30 +0200

    Add Matomo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,12 @@
 <script>
 	import Header from '$components/header/Header.svelte'
 	import Footer from '$components/Footer.svelte'
+	import Matomo from '$components/Matomo.svelte'
+
 	import '$style/main.scss'
 </script>
+
+<Matomo />
 
 <Header />
 
