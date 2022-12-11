58cb8dde57260e6a374fbb64027c30991e6a3b34
Džuris <juris@glaive.pro>
2022-12-11 13:01:19 +0200

    Use nativer Matomo integration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,9 @@
 <script>
 	import Header from '$components/header/Header.svelte'
 	import Footer from '$components/Footer.svelte'
-	import Matomo from '$components/Matomo.svelte'
 
 	import '$style/main.scss'
 </script>
-
-<Matomo />
 
 <Header />
 
