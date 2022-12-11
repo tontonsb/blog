79f1d94af9ec671bbeaa5615ad06143f833bcf7b
Džuris <juris@glaive.pro>
2022-12-11 12:34:34 +0200

    Use native Fathom

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,11 @@
 	import Header from '$components/header/Header.svelte'
 	import Footer from '$components/Footer.svelte'
 	import Matomo from '$components/Matomo.svelte'
-	import Fathom from '$components/Fathom.svelte'
 
 	import '$style/main.scss'
 </script>
 
 <Matomo />
-<Fathom />
 
 <Header />
 
