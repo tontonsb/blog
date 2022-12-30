84f0cbfc07fcbddb177b56f83d237cce2ba8fea2
Džuris <juris@glaive.pro>
2022-12-30 17:45:07 +0200

    Add simple tests

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,34 @@
-import { expect, test } from '@playwright/test';
+import { expect, test } from '@playwright/test'
 
-test('about page has expected h1', async ({ page }) => {
-	await page.goto('/about');
-	expect(await page.textContent('h1')).toBe('About this app');
-});
+// Check pages in parallel
+test.describe.configure({ mode: 'parallel' })
+
+test('index page works', async ({ page }) => {
+	await page.goto('/')
+
+	expect(await page.textContent('h1')).toBe('Nestandarta analīze')
+})
+
+test('blog page works', async ({ page }) => {
+	await page.goto('/blog')
+
+	expect(await page.textContent('h1')).toBe('Raksti')
+})
+
+test('privacy page works', async ({ page }) => {
+	await page.goto('/privacy')
+
+	expect(await page.textContent('h1')).toBe('Datu politika')
+})
+
+test('some article works', async ({ page }) => {
+	await page.goto('/blog/iepirkumi')
+
+	expect(await page.textContent('h1')).toBe('Valsts iepirkumi')
+})
+
+test('calc page works', async ({ page }) => {
+	await page.goto('/iepirkumu-kalkulators')
+
+	expect(await page.textContent('h1')).toBe('Iepirkumu kalkulators')
+})
