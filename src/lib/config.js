4b9b65f72ba8683326e1bbee27377fc68c6d4439
Džuris <juris@glaive.pro>
2023-01-15 15:03:47 +0200

    Add explicit exports from config

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,4 +1,7 @@
+export const title = 'Nestandarta analīze'
+export const description = 'Šeit mēs runāsim par bezgalīgi maziem un bezgalīgi lieliem lielumiem. Kā pie īstiem vīriem. Kā Leibnics nevis Veierštrāss.'
+
 export default {
-	title: 'Nestandarta analīze',
-	description: 'Šeit mēs runāsim par bezgalīgi maziem un bezgalīgi lieliem lielumiem. Kā pie īstiem vīriem. Kā Leibnics nevis Veierštrāss.',
+	title,
+	description,
 }
