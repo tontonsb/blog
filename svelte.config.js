58f57b39760d5ba69d5f63d9a51740efb222ca8c
Džuris <juris@glaive.pro>
2026-03-14 16:05:42 +0200

    Modernize tool usage

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import adapter from '@sveltejs/adapter-static'
 import { mdsvex } from 'mdsvex'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypeSlug from 'rehype-slug'
-import sveltePreprocess from 'svelte-preprocess'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -17,7 +17,7 @@ const config = {
 	},
 	extensions: ['.svelte', '.md'],
 	preprocess: [
-		sveltePreprocess(),
+		vitePreprocess(),
 		mdsvex({
 			extensions: ['.md'],
 			remarkPlugins: [
