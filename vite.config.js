fb66d5ff6e7a9a05f7237bc62b5f7829bedad885
Džuris <juris@glaive.pro>
2026-03-14 18:10:46 +0200

    Make the build faster

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
+	build: {
+		reportCompressedSize: false, // Improves build speed a lot
+	},
 };
 
 export default config;
